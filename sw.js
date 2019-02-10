6fe779fa9e6127dcd1a412471c1f3b727cd4471d
gwinbleid <corsar909@gmail.com>
2019-02-10 17:20:37 +0300

    test 3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-self.addEventListener('install', function(event) {
+this.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open('v1').then(function(cache) {
             return cache.addAll([
@@ -14,7 +14,7 @@ self.addEventListener('install', function(event) {
     );
 });
 
-self.addEventListener('fetch', function(event) {
+this.addEventListener('fetch', function(event) {
     event.respondWith(caches.match(event.request).then(function(response) {
         if (response !== undefined) {
             return response;
